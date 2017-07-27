3cd1f9647def8f31121948c8cc1009e7902fa79e
Manuel Selbach <m.selbach@syzygy.de>
2017-07-27 10:05:55 +0200

    Add version 2.0.0 form TER

---
+++ ext_emconf.php
@@ -0,0 +1,39 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "static_info_tables_ja".
+#
+# Auto generated 17-09-2015 06:54
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Static Info Tables (ja)',
+	'description' => '(ja) language pack for the Static Info Tables providing localized names for countries, currencies and so on.',
+	'category' => 'misc',
+	'shy' => 0,
+	'version' => '2.0.0',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'TYPO3_version' => '4.0-0.0.0',
+	'PHP_version' => '4.1.0-0.0.0',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => 'static_countries,static_languages,static_currencies,static_territories',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Manuel Rego Casasnovas [Igalia]',
+	'author_email' => 'mrego@igalia.com',
+	'author_company' => 'Igalia - http://www.igalia.com',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+);
+
+?>
