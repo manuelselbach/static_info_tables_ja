f3c82a54c68143f8fe62cbe9d514317208b58681
Manuel Selbach <manuel_selbach@yahoo.de>
2018-07-05 23:36:05 +0200

    [TASK] Update for compatibility with static_info_tables 6.5
    close #2

--- class.ext_update.php
+++ class.ext_update.php
@@ -43,7 +43,6 @@ class ext_update
      */
     public function main()
     {
-        $content = '';
         $objectManager = GeneralUtility::makeInstance(ObjectManager::class);
 
         // Clear the class cache
@@ -57,8 +56,7 @@ class ext_update
         $databaseUpdateUtility->doUpdate(Extension::EXTENSION_KEY);
 
         $updateLanguageLabels = LocalizationUtility::translate('updateLanguageLabels', 'StaticInfoTables');
-        $content.= '<p>' . $updateLanguageLabels . ' '. Extension::EXTENSION_KEY . '</p>';
-        return $content;
+        return '<p>' . $updateLanguageLabels . ' '. Extension::EXTENSION_KEY . '</p>';
     }
 
     /**
